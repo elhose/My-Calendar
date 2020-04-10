50515b328b707868f76b212ddd75c97e48ef6caf
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-04-10 20:09:47 +0200

    added base controller and made rest controllers generic

--- src/main/java/com/js/calendar/controller/DayController.java
+++ src/main/java/com/js/calendar/controller/DayController.java
@@ -1,64 +1,20 @@
 package com.js.calendar.controller;
 
-import com.js.calendar.dto.DayDTO;
-import com.js.calendar.dto.DayUpdateDTO;
-import com.js.calendar.entities.Day;
 import com.js.calendar.mappers.DayMapper;
 import com.js.calendar.service.DayService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("days")
-public class DayController {
+public class DayController extends BaseController{
 
     private DayMapper mapper;
     private DayService service;
 
     public DayController(DayMapper mapper, DayService service) {
+        super(mapper, service);
         this.mapper = mapper;
         this.service = service;
-    }
-
-    @GetMapping
-    public ResponseEntity<Iterable<DayDTO>> getDays() {
-        Iterable<Day> days = service.getEntities();
-        List<DayDTO> dtos = new ArrayList<>();
-
-        days.forEach(day -> dtos.add(mapper.mapDayToDto(day)));
-
-        return new ResponseEntity<>(dtos, HttpStatus.OK);
-    }
-
-    @GetMapping("/{dayId}")
-    public ResponseEntity<DayDTO> getDayById(@PathVariable("dayId") Long dayId) {
-        Optional<Day> day = service.getEntity(dayId);
-
-        return day.map(value -> new ResponseEntity<>(mapper.mapDayToDto(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(new DayDTO(), HttpStatus.NOT_FOUND));
-    }
-
-    @PostMapping
-    public ResponseEntity addNewDay(@RequestBody DayUpdateDTO dayUpdateDTO) {
-        Day day = mapper.mapDayUpdateDtoToDay(dayUpdateDTO);
-        service.addEntity(day);
-        return new ResponseEntity(HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{dayId}")
-    public ResponseEntity updateUser(@PathVariable("dayId") Long dayId, @RequestBody DayUpdateDTO dayUpdateDTO) {
-        Day day = mapper.mapDayUpdateDtoToDay(dayUpdateDTO);
-        service.updateEntity(dayId, day);
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{dayId}")
-    public ResponseEntity deleteUser(@PathVariable("dayId") Long dayId) {
-        service.deleteEntity(dayId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/js/calendar/controller/UserController.java
+++ src/main/java/com/js/calendar/controller/UserController.java
@@ -1,66 +1,22 @@
 package com.js.calendar.controller;
 
-import com.js.calendar.dto.UserDTO;
-import com.js.calendar.dto.UserUpdateDTO;
-import com.js.calendar.entities.User;
 import com.js.calendar.mappers.UserMapper;
 import com.js.calendar.service.UserService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
-public class UserController {
+public class UserController extends BaseController{
 
     private UserMapper mapper;
     private UserService service;
 
     public UserController(UserMapper mapper, UserService service) {
+        super(mapper, service);
         this.mapper = mapper;
         this.service = service;
     }
 
-    @GetMapping
-    public ResponseEntity<Iterable<UserDTO>> getUsers() {
-        Iterable<User> users = service.getEntities();
-        List<UserDTO> dtos = new ArrayList<>();
 
-        users.forEach(user -> dtos.add(mapper.mapUserToUserDTO(user)));
-
-        return new ResponseEntity<>(dtos, HttpStatus.OK);
-    }
-
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable("userId") Long userId) {
-        Optional<User> user = service.getEntity(userId);
-
-        return user.map(value -> new ResponseEntity<>(mapper.mapUserToUserDTO(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(new UserDTO(), HttpStatus.NOT_FOUND));
-    }
-
-    @PostMapping
-    public ResponseEntity addNewUser(@RequestBody UserUpdateDTO userUpdateDTO) {
-        User user = mapper.mapUserUpdateDtoToUser(userUpdateDTO);
-        user.setEnabled(true);
-        service.addEntity(user);
-        return new ResponseEntity(HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{userId}")
-    public ResponseEntity updateUser(@PathVariable("userId") Long userId, @RequestBody UserUpdateDTO userUpdateDTO) {
-        User user = mapper.mapUserUpdateDtoToUser(userUpdateDTO);
-        user.setEnabled(true);
-        service.updateEntity(userId, user);
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{userId}")
-    public ResponseEntity deleteUser(@PathVariable("userId") Long userId) {
-        service.deleteEntity(userId);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
 }
