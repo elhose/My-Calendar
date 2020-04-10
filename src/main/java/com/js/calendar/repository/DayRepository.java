3233018c2ab11f988e1e0203ff2a269e82039176
Jacek Sawko <jacek.sawko@ttpsc.pl>
2020-04-10 14:12:24 +0200

    added Base Repository and left custom ones, allowing for future, entity specific queries

--- src/main/java/com/js/calendar/repository/DayRepository.java
+++ src/main/java/com/js/calendar/repository/DayRepository.java
@@ -1,10 +1,9 @@
 package com.js.calendar.repository;
 
 import com.js.calendar.entities.Day;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DayRepository extends PagingAndSortingRepository<Day, Long> {
+public interface DayRepository extends BaseRepository<Day> {
 
 }
